c602bc40c8c2b15ca8fb411fd1de81a5a46c89f1
Leeroy Amador <leeroy.amador@outlook.com>
2024-10-04 10:51:59 +1000

    updates

--- projectfile/website/models.py
+++ projectfile/website/models.py
@@ -10,7 +10,6 @@ class User(db.Model, UserMixin):
 	#password is never stored in the DB, an encrypted password is stored
 	# the storage should be at least 255 chars long
     password_hash = db.Column(db.String(255), nullable=False)
-    # relation to call user.comments and comment.created_by
     comments = db.relationship('Comment', backref='user')
 
     # string print method
@@ -19,6 +18,17 @@ class User(db.Model, UserMixin):
 
 
 class Event(db.Model):
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    description = db.Column(db.String(200))
+    image = db.Column(db.String(400))
+    currency = db.Column(db.String(3))
+    comments = db.relationship('Comment', backref='event')
+
+	# string print method
+    def __repr__(self):
+        return f"Name: {self.name}"
 
 class Comment(db.Model):
     __tablename__ = 'comments'
@@ -27,9 +37,9 @@ class Comment(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.now())
     # add the foreign keys
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    destination_id = db.Column(db.Integer, db.ForeignKey('events.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
 
     def __repr__(self):
         return f"Comment: {self.text}"
 
-class Order(db.Model):
+## class Order(db.Model):
