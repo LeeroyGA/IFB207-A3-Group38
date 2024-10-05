726568dbffdb2ecf93b7eb4a9a3e84cd85b9cb7f
Leeroy Amador <leeroy.amador@outlook.com>
2024-10-05 20:04:40 +1000

    updates

--- a3_group38/sportspass/models.py
+++ a3_group38/sportspass/models.py
@@ -3,9 +3,46 @@ from datetime import datetime
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'users' # good practice to specify table name
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), index=True, unique=True, nullable=False)
+    emailid = db.Column(db.String(100), index=True, nullable=False)
+	#password is never stored in the DB, an encrypted password is stored
+	# the storage should be at least 255 chars long
+    password_hash = db.Column(db.String(255), nullable=False)
+    # relation to call user.comments and comment.created_by
+    comments = db.relationship('Comment', backref='user')
+
+    # string print method
+    def __repr__(self):
+        return f"Name: {self.name}"
 
 class Event(db.Model):
-
+    __tablename__ = 'events'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80))
+    description = db.Column(db.String(200))
+    date = db.Column(db.DateTime, nullable=False)
+    location = db.Column(db.String(200), nullable=False)
+    image = db.Column(db.String(400))
+    capacity = db.Column(db.Integer, nullable=False)
+    # ... Create the Comments db.relationship
+	# relation to call event.comments and comment.event
+    comments = db.relationship('Comment', backref='event')
+	# string print method
+    def __repr__(self):
+        return f"Name: {self.name}"
+    
 class Comment(db.Model):
+    __tablename__ = 'comments'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(400))
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    # add the foreign keys
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'))
+
+    def __repr__(self):
+        return f"Comment: {self.text}"
 
 class Order(db.Model):
