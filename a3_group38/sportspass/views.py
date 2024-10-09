0e7112c08eeac25da8aff3af852f14338fa1fb35
Leeroy Amador <leeroy.amador@outlook.com>
2024-10-09 14:12:40 +1000

    Update to views.py

--- a3_group38/sportspass/views.py
+++ a3_group38/sportspass/views.py
@@ -1,14 +1,13 @@
 from flask import Blueprint, render_template, request, redirect, url_for
-from .models import User, Event
+from .models import Event
+
+#from .models import Destination
+from .models import User
 from . import db
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
-    return render_template('index.html')
-
-@main_bp.route('/create-event', methods=['GET', 'POST'])
-def create_event():
-    # Your function logic here
-    return render_template('eventCreation.html')
+    events = db.session.scalars(db.select(Event)).all()    
+    return render_template('index.html', events=events)
