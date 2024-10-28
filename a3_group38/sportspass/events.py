92ceab924cd6fc923eff0a2e44864352d190e583
Hacklings <jamesgolding114@gmail.com>
2024-10-28 13:05:33 +1000

    Update events.py

--- a3_group38/sportspass/events.py
+++ a3_group38/sportspass/events.py
@@ -1,10 +1,9 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from .models import Event, Comment
-from .forms import EventForm, CommentForm
+from .forms import EventForm, CommentForm, UpdateEventForm, CancelEventForm
 from . import db
 import os
 from werkzeug.utils import secure_filename
-#additional import:
 from flask_login import login_required, current_user
 
 eventbp = Blueprint('event', __name__, url_prefix='/events')
@@ -12,60 +11,92 @@ eventbp = Blueprint('event', __name__, url_prefix='/events')
 @eventbp.route('/<id>')
 def show(id):
     event = db.session.scalar(db.select(Event).where(Event.id==id))
-    # create the comment form
-    form = CommentForm()    
+    form = CommentForm()
     return render_template('events/show.html', event=event, form=form)
 
 # create a new event function
 @eventbp.route('/create', methods=['GET', 'POST'])
 @login_required
 def create():
-  print('Method type: ', request.method)
-  form = EventForm()
-  if form.validate_on_submit():
-    #call the function that checks and returns image
-    db_file_path = check_upload_file(form)
-    event = Event(name=form.name.data, description=form.description.data, date=form.date.data, location=form.location.data, image=db_file_path, capacity=form.capacity.data, price=form.price.data, category=form.category.data, status='open', user_id=current_user.id)
-    # add the object to the db session
-    db.session.add(event)
-    # commit to the database
-    db.session.commit()
-    flash('Successfully created new event', 'success')
-    #Always end with redirect when form is valid
-    return redirect(url_for('event.create'))
-  return render_template('events/create.html', form=form)
+    print('Method type: ', request.method)
+    form = EventForm()
+    if form.validate_on_submit():
+        db_file_path = check_upload_file(form)
+        event = Event(
+            name=form.name.data,
+            description=form.description.data,
+            date=form.date.data,
+            location=form.location.data,
+            image=db_file_path,
+            capacity=form.capacity.data,
+            price=form.price.data,
+            category=form.category.data,
+            status='open',
+            user_id=current_user.id
+        )
+        db.session.add(event)
+        db.session.commit()
+        flash('Successfully created new event', 'success')
+        return redirect(url_for('event.create'))
+    return render_template('events/create.html', form=form)
 
+# helper function to check and save uploaded file
 def check_upload_file(form):
-  #get file data from form  
-  fp = form.image.data
-  filename = fp.filename
-  #get the current path of the module file… store image file relative to this path  
-  BASE_PATH = os.path.dirname(__file__)
-  #upload file location – directory of this file/static/image
-  upload_path = os.path.join(BASE_PATH, 'static/img', secure_filename(filename))
-  #store relative path in DB as image location in HTML is relative
-  db_upload_path = '/static/img/' + secure_filename(filename)
-  #save the file and return the db upload path
-  fp.save(upload_path)
-  return db_upload_path
+    fp = form.image.data
+    filename = fp.filename
+    BASE_PATH = os.path.dirname(__file__)
+    upload_path = os.path.join(BASE_PATH, 'static/img', secure_filename(filename))
+    db_upload_path = '/static/img/' + secure_filename(filename)
+    fp.save(upload_path)
+    return db_upload_path
 
 # comment on an event function
-@eventbp.route('/<id>/comment', methods=['GET', 'POST'])  
+@eventbp.route('/<id>/comment', methods=['GET', 'POST'])
 @login_required
-def comment(id):  
-    form = CommentForm()  
-    #get the event object associated to the page and the comment
+def comment(id):
+    form = CommentForm()
     event = db.session.scalar(db.select(Event).where(Event.id==id))
-    if form.validate_on_submit():  
-      #read the comment from the form
-      comment = Comment(text=form.text.data, event=event,
-                        user=current_user) 
-      #here the back-referencing works - comment.event is set
-      # and the link is created
-      db.session.add(comment) 
-      db.session.commit() 
-      #flashing a message which needs to be handled by the html
-      flash('Your comment has been added', 'success')  
-      # print('Your comment has been added', 'success') 
-    # using redirect sends a GET request to event.show
+    if form.validate_on_submit():
+        comment = Comment(text=form.text.data, event=event, user=current_user)
+        db.session.add(comment)
+        db.session.commit()
+        flash('Your comment has been added', 'success')
+    return redirect(url_for('event.show', id=id))
+
+# update event function
+@eventbp.route('/<int:id>/update', methods=['GET', 'POST'])
+@login_required
+def update_event(id):
+    event = db.session.scalar(db.select(Event).where(Event.id == id))
+    if event.user_id != current_user.id:
+        flash("You are not authorized to update this event.", "danger")
+        return redirect(url_for('event.show', id=id))
+
+    form = UpdateEventForm(obj=event)
+    if form.validate_on_submit():
+        event.name = form.name.data
+        event.description = form.description.data
+        event.date = form.date.data
+        event.location = form.location.data
+        event.price = form.price.data
+        event.capacity = form.capacity.data
+        event.category = form.category.data
+        db.session.commit()
+        flash("Event updated successfully.", "success")
+        return redirect(url_for('event.show', id=id))
+    
+    return render_template('events/update.html', form=form, event=event)
+
+# cancel event function
+@eventbp.route('/<int:id>/cancel', methods=['POST'])
+@login_required
+def cancel_event(id):
+    event = db.session.scalar(db.select(Event).where(Event.id == id))
+    if event.user_id != current_user.id:
+        flash("You are not authorized to cancel this event.", "danger")
+        return redirect(url_for('event.show', id=id))
+
+    event.status = 'cancelled'
+    db.session.commit()
+    flash("Event has been cancelled.", "success")
     return redirect(url_for('event.show', id=id))
